7f7d4611692026d532166d019bceb54a10668802
Vladimir Roncevic <elektron.ronca@gmail.com>
2017-02-13 20:10:11 +0100

    Initial code

---
+++ bin/GenGTKWidget.pm
@@ -0,0 +1,148 @@
+package GenGTKWidget;
+#
+# @brief    Generate GTK widget module source files
+# @version  ver.1.0
+# @date     Mon Dec 26 10:04:59 CET 2016
+# @company  None, free software to use 2016
+# @author   Vladimir Roncevic <elektron.ronca@gmail.com>
+#
+use strict;
+use warnings;
+use Exporter;
+use Sys::Hostname;
+use Template;
+use POSIX qw(strftime);
+use Cwd qw(abs_path getcwd);
+use File::Basename qw(dirname);
+use lib dirname(dirname(abs_path(__FILE__))) . '/../../lib/perl5';
+use InfoDebugMessage qw(info_debug_message);
+use InfoMessage qw(info_message);
+use Configuration qw(read_preference);
+use Notification qw(notify);
+use Logging qw(logging);
+use Status;
+our @ISA = qw(Exporter);
+our %EXPORT_TAGS = ('all' => [qw()]);
+our @EXPORT_OK = (@{$EXPORT_TAGS{'all'}});
+our @EXPORT = qw(gen_gtk_widget);
+our $VERSION = '1.0';
+our $TOOL_DBG = "false";
+
+#
+# @brief   Generate GTK widget module source files
+# @param   Value required hash argument structure with parameters
+# @exitval Function gen_gtk_widget exit with integer value
+#			0   - success operation
+#			128 - check argument option structure
+#			129 - failed to read configuration file
+#			130 - check template files
+#
+# @usage
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+#
+# gen_gtk_widget(\%option)
+#
+sub gen_gtk_widget {
+	my %opt = %{$_[0]};
+	my $msg = "None";
+	if(%opt) {
+		$msg = "Generate GTK widget [$opt{WIDGET_NAME}]";
+		info_message($msg);
+		my ($cfg, $log, %preferences);
+		$cfg = dirname(dirname(abs_path(__FILE__))) . "/conf/gengtkwidget.cfg";
+		$log = dirname(dirname(abs_path(__FILE__))) . "/log/gengtkwidget.log";
+		if(read_preference($cfg, \%preferences) == $SUCCESS) {
+			my ($CCT, $HCT);
+			$CCT = dirname(dirname(abs_path(__FILE__))) . "$preferences{CCT}";
+			$HCT = dirname(dirname(abs_path(__FILE__))) . "$preferences{HCT}";
+			$msg = "Setup template files:\nCCT: $CCT\n HCT: $HCT";
+			info_debug_message($msg);
+			if((-e "$CCT") && (-e "$HCT")) {
+				my (%templateParams, $ucwn, $lcwn, $year);
+				$ucwn = uc($opt{WIDGET_NAME});
+				$lcwn = lc($opt{WIDGET_NAME});
+				$year = strftime("%Y", localtime());
+				%templateParams = (
+					PN => $opt{PROJECT_NAME}, WN => $opt{WIDGET_NAME},
+					WNUC => $ucwn, WNLC => $lcwn, YR => $year,
+					AN => $preferences{AUTHOR_NAME},
+					AE => $preferences{AUTHOR_EMAIL}
+				);
+				my ($templateGen, $cct, $cfh, $hct, $hfh);
+				$templateGen = Template->new(ABSOLUTE => 1);
+				$templateGen->process("$CCT", \%templateParams, \$cct) ||
+					die($templateGen->error);
+				$templateGen->process("$HCT", \%templateParams, \$hct) ||
+					die($templateGen->error);
+				$msg = "Processing widget parameters and generate text code";
+				info_debug_message($msg);
+				my ($currentDir, $uid, $gid, %log);
+				$currentDir = getcwd();
+				$uid = getpwnam("$preferences{UID}");
+				$gid = getgrnam("$preferences{GID}");
+				$msg = "Generating widget module files:\n";
+				$msg .= "\t$currentDir/$lcwn.h\n";
+				$msg .= "\t$currentDir/$lcwn.c";
+				info_message($msg);
+				open($hfh, '>', "$currentDir/$lcwn.h");
+				print($hfh "$hct");
+				close($hfh);
+				chown($uid, $gid, "$currentDir/$lcwn.h");
+				open($cfh, '>', "$currentDir/$lcwn.c");
+				print($cfh "$cct");
+				close($cfh);
+				chown($uid, $gid, "$currentDir/$lcwn.c");
+				$log{LOG_FILE_PATH} = $log;
+				$log{LOG_MESSAGE} = "Generated GTK widget [$opt{WIDGET_NAME}]";
+				logging(\%log);
+				$msg = "Done";
+				info_message($msg);
+				exit(0);
+			}
+			$msg = "Check files:\n\tCCT [$CCT]\n\tHCT [$HCT]";
+			error_message($msg);
+			exit(130);
+		}
+		exit(129);
+	}
+	$msg = "Missing argument [OPTION_STRUCTURE]";
+	error_message($msg);
+	exit(128);
+}
+
+1;
+__END__
+
+=head1 NAME
+
+GenGTKWidget - This module is for generating GTK widget module source files.
+
+=head1 SYNOPSIS
+
+	use GenGTKWidget qw(gen_gtk_widget);
+
+	...
+
+	gen_gtk_widget(\%option);
+
+=head1 DESCRIPTION 
+
+This script is for generating GTK widget module source files.
+
+=head2 EXPORT
+
+gen_gtk_widget - Success exit with code 0, else none 0
+
+=head1 AUTHOR
+
+Vladimir Roncevic, E<lt>elektron.ronca@gmail.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2016 by https://github.com/vroncevic/gen_gtk_widget
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.18.2 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
---
+++ bin/gen_gtk_widget_run.pl
@@ -0,0 +1,149 @@
+#!/usr/bin/env perl
+#
+# @brief    Generate GTK widget module source files
+# @version  ver.1.0
+# @date     Mon Dec 26 10:04:59 CET 2016
+# @company  None, free software to use 2016
+# @author   Vladimir Roncevic <elektron.ronca@gmail.com>
+#
+use strict;
+use warnings;
+use Getopt::Long;
+use Pod::Usage;
+use Cwd qw(abs_path);
+use File::Basename qw(dirname);
+use lib dirname(dirname(abs_path(__FILE__))) . '/bin';
+use GenGTKWidget qw(gen_gtk_widget);
+use lib dirname(dirname(abs_path(__FILE__))) . '/../../lib/perl5';
+use CheckStatus qw(check_status);
+use Utils qw(def);
+use Status;
+our $TOOL_DBG = "false";
+
+#
+# @brief   Main entry point
+# @params  Values required project name and widget name, optional help | manual
+# @exitval Script tool gengtkwidget exit with integer value
+#			0   - success operation
+#			127 - run as root user
+#			128 - check argument option structure
+#			129 - failed to read configuration file
+#			130 - check template files
+#
+my (%option, $projectName, $widgetName, $help, $man);
+
+if(@ARGV > 0) {
+	GetOptions(
+		'pname=s' => \$projectName, 'wname=s' => \$widgetName,
+		'help|?'  => \$help, 'manual'  => \$man
+	) || pod2usage(2);
+}
+
+my $username = (getpwuid($>));
+my $uid = ($<);
+
+if(($username eq "root") && ($uid == 0)) {
+	if(def($help) == $SUCCESS) {
+		pod2usage(1);
+	} elsif(def($man) == $SUCCESS) {
+		pod2usage(VERBOSE => 2);
+	} else {
+		my %status = (
+			projectName => def($projectName), widgetName => def($widgetName)
+		);
+		if(check_status(\%status) == $SUCCESS) {
+			$option{PROJECT_NAME} = $projectName;
+			$option{WIDGET_NAME} = $widgetName;
+			gen_gtk_widget(\%option);
+		} else {
+			pod2usage(1);
+		}
+	}
+}
+exit(127);
+
+__END__
+
+############################ POD gengtkwidget.pl ###############################
+
+=head1 NAME
+
+gengtkwidget - Generate GTK widget module source files
+
+=head1 SYNOPSIS
+
+Use:
+
+	gengtkwidget [options]
+
+	[options] pname wname help manual
+
+Examples:
+
+	# Generate GTK widget module source files example
+	gengtkwidget --pname MyProject --wname MyWidget
+
+	# Print this option
+	gengtkwidget --help
+
+	# Print code of tool
+	gengtkwidget --manual
+
+	# Return values
+	0   - success operation 
+	127 - run as root user
+	128 - check argument option structure
+	129 - failed to read configuration file
+	130 - check template files
+
+=head1 DESCRIPTION
+
+This script is for generating GTK widget module source files.
+
+=head1 ARGUMENTS
+
+gengtkwidget takes the following arguments:
+
+=over 4
+
+=item pname
+
+	pname
+
+	(Required.) Project name (destination of GTK module)
+
+=item wname
+
+	wname
+
+	(Required.) Widget name
+
+=item help
+
+	help
+
+	(Optional.) Show help info information
+
+=item manual
+
+	manual
+
+	(Optional.) Display manual
+
+=back
+
+=head1 AUTHORS
+
+Vladimir Roncevic, E<lt>elektron.ronca@gmail.comE<gt>.
+
+=head1 COPYRIGHT
+
+	Free software to use 2016.
+
+=head1 DATE
+
+26-Dec-2016
+
+=cut
+
+############################ POD gengtkwidget.pl ###############################
